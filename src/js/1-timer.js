1823f367aaf326a6f09e6391850c2cb35e2376cd
Elizabeth Shkinder <elizabethshkinder@unknown.home>
2025-10-26 12:09:02 +0000

    update header.html, add timer markup, install flatpickr, add link to html elements

---
+++ src/js/1-timer.js
@@ -0,0 +1,22 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+flatpickr(input, options);
+
+const input = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('[data-start]');
+const daysEl = document.querySelector('[data-days]');
+const hoursEl = document.querySelector('[data-hours]');
+const minutesEl = document.querySelector('[data-minutes]');
+const secondsEl = document.querySelector('[data-seconds]')
+
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
