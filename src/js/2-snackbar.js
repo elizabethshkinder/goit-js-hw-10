73791bfb83b846441dd4cbe7791aec45175a5ead
Elizabeth Shkinder <elizabethshkinder@unknown.home>
2025-10-28 11:44:34 +0000

    add js logic and update styles for task-2

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,2 +1,44 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const formData = new FormData(form);
+
+    const delay = Number(formData.get('delay'));
+    const state = formData.get('state');
+
+    const promise = new Promise ((resolve, reject) => {
+        setTimeout(() => {
+            if(state === 'fulfilled') {
+                resolve(delay);
+            } else {
+                reject(delay);
+            }
+        }, delay);
+    })
+
+    promise
+    .then(delayValue => {
+        iziToast.show({
+            message: `✅ Fulfilled promise in ${delayValue}ms`,
+            messageColor: '#ffffff',
+            position: 'topRight',
+            class: 'toast-success',
+        });
+    })
+    .catch(delayValue => {
+        iziToast.show({
+            message: `❌ Rejected promise in ${delayValue}ms`,
+            messageColor: '#ffffff', 
+            position: 'topRight',
+            class: 'toast-error',
+        })
+    });
+
+});
+
+
